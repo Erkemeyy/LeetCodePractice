2bd93312c2a5ab91927c5a760b10a195eb093b38
Erkemeyy <ilinartem293@gmail.com>
2025-11-21 18:30:40 +0200

    crazy loop

--- easy/src/GreatestCommonDivisorOfStrings.java
+++ easy/src/GreatestCommonDivisorOfStrings.java
@@ -1,34 +1,35 @@
 public class GreatestCommonDivisorOfStrings {
     public static void main (String[] args) {
         GreatestCommonDivisorOfStrings gcd = new GreatestCommonDivisorOfStrings();
-        System.out.println(gcd.gcdOfStrings("ABCABC", "ABC"));
+        System.out.println(gcd.gcdOfStrings("BACABC", "ABC"));
 
     }
     public String gcdOfStrings(String str1, String str2) {
-        String shortest = "";
         StringBuilder newStr = new  StringBuilder();
-        if(str1.length()>str2.length()){
-            shortest = str2;
-        }
-        else{
-            shortest = str1;
-        }
-        for (int i = 0; i < shortest.length(); i++) {
-            if (shortest.charAt(i) == shortest.charAt(0) && i > 0){
+        int x = str1.length();
+        int y = str2.length();
+
+
+        for (int i = 0; i < y; i++) {
+            if (x-y > 4 && str1.charAt(i+1) == str1.charAt(1) && i > 0){
+                newStr = newStr.replace(0, newStr.length(), "");
                 break;
             }
-
-            if (shortest.charAt(i) == str2.charAt(i)) {
-
-                newStr.append(shortest.charAt(i));
+            if (str1.charAt(i) == str1.charAt(0) && i > 0 && x%y!=0) {
+                break;
             }
+            if (str1.charAt(i) == str2.charAt(i) && str1.charAt(x-1) == str2.charAt(y-1)) {
+                newStr.append(str1.charAt(i));
+            } else {
+                break;
+            }
+            if (x>y && str1.charAt(y) != str2.charAt(0)) {
+                newStr = newStr.replace(0, newStr.length(), "");
 
-
-
-
+                break;
+            }
         }
-        String string1 = newStr.toString();
-        return string1;
+        return newStr.toString();
 
     }
 }
