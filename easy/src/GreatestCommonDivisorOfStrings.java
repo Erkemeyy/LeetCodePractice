18912590e41bfbcf0c4cbf29e8044496493526ca
Erkemeyy <ilinartem293@gmail.com>
2025-11-21 20:27:06 +0200

    almost

--- easy/src/GreatestCommonDivisorOfStrings.java
+++ easy/src/GreatestCommonDivisorOfStrings.java
@@ -1,35 +1,51 @@
 public class GreatestCommonDivisorOfStrings {
     public static void main (String[] args) {
         GreatestCommonDivisorOfStrings gcd = new GreatestCommonDivisorOfStrings();
-        System.out.println(gcd.gcdOfStrings("BACABC", "ABC"));
+        System.out.println(gcd.gcdOfStrings("ABABABAB", "AB"));
+        StringBuilder test = new StringBuilder("a");
+        System.out.println(test.toString() == "a");
 
     }
     public String gcdOfStrings(String str1, String str2) {
-        StringBuilder newStr = new  StringBuilder();
+        StringBuilder newStr = new StringBuilder();
         int x = str1.length();
         int y = str2.length();
+        int smallNum =0;
+        int largestNum = 0;
 
 
-        for (int i = 0; i < y; i++) {
-            if (x-y > 4 && str1.charAt(i+1) == str1.charAt(1) && i > 0){
-                newStr = newStr.replace(0, newStr.length(), "");
-                break;
-            }
-            if (str1.charAt(i) == str1.charAt(0) && i > 0 && x%y!=0) {
-                break;
-            }
-            if (str1.charAt(i) == str2.charAt(i) && str1.charAt(x-1) == str2.charAt(y-1)) {
-                newStr.append(str1.charAt(i));
-            } else {
-                break;
-            }
-            if (x>y && str1.charAt(y) != str2.charAt(0)) {
-                newStr = newStr.replace(0, newStr.length(), "");
 
-                break;
-            }
+        if (x > y){
+            smallNum = y;
+            largestNum = x;
+        } else{
+            smallNum = x;
+            largestNum = y;
         }
+
+        if (largestNum % smallNum ==0 ){
+            String newStr1 = "";
+
+            while(!newStr.toString().equals(str1)){
+                int i =0;
+                if(str1.charAt(0) == str2.charAt(0)){
+                    newStr.append(str1.charAt(0));
+                    str1.replace(String.valueOf(str2.charAt(0)), "");
+                }
+
+
+
+            }
+
+
+
+        } else {
+            newStr = new StringBuilder("");
+        }
+
         return newStr.toString();
+
+
 
     }
 }
